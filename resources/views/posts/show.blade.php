82366e3706828e418451c2ef1e7906aed5b9c69d
IlijaT <tatalovicilij@gmail.com>
2018-11-22 18:33:33 +0100

    Add Comments Feature

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,59 @@
 @extends('layouts.master')
 
 @section('content')
-
-  <div class="col-md-8 blog-main">
+  <div class="col-md-8 blog-post">
     
     <h1>{{ $post->title }}</h1>
 
     <p>{{ $post->body }}</p>
+
+    <hr>
+
+    <div class="comments">
+
+      @foreach($post->comments as $comment)
+       <ul class="list-group">
+
+         <li class="list-group-item">
+
+          <strong>
+
+              {{ $comment->created_at->diffForHumans() }}: &nbsp;
+
+          </strong>
+         
+
+          {{ $comment->body }} 
+
+         </li>
+
+       </ul>
+      @endforeach
+  
+    </div>
+
+    <hr>
+
+    <div>
+
+      <form method="POST" action="/posts/{{ $post->id }}/comments">
+
+        @csrf
+  
+        <div class="form-group">
+          <textarea class="form-control" placeholder="Your comment here..." id="body" name="body" rows="3" required></textarea>
+        </div>
+  
+        <div class="form-group">
+          <button type="submit" class="btn btn-dark">Add Comment</button>
+        </div>
+        
+      </form>
+
+      @include('layouts.errors')
+
+
+    </div>
     
   </div>
-
 @endsection
